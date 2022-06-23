c00be00ceb9af62483c4c3adbf53d46aaab7b702
Andrzej K <32792205+Razz21@users.noreply.github.com>
2022-06-23 21:40:25 +0200

    chore: cors preflight request

--- lambda/redirect.js
+++ lambda/redirect.js
@@ -1,4 +1,16 @@
 exports.handler = async (event, context) => {
+  if (event.httpMethod==="OPTIONS") {
+    const headers = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, POST',
+    };
+    return {
+      statusCode: 200,
+      headers,
+      body: 'success',
+    }
+  }
   return {
     statusCode: 307,
     headers: {

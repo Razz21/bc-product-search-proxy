6011653dbbab0828fd53f1787f9e25c18333969f
Andrzej K <32792205+Razz21@users.noreply.github.com>
2022-06-24 00:20:48 +0200

    chore: options headers

--- express/server.js
+++ express/server.js
@@ -5,10 +5,15 @@ const router = require('./router')
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.options(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/.netlify/functions/api', router);
 
 module.exports = app;

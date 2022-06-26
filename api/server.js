d17612065fb32ddeda65d97fbc76e271b29dd10f
Andrzej K <32792205+Razz21@users.noreply.github.com>
2022-06-26 23:13:48 +0200

    chore: update api to aws

--- api/server.js
+++ api/server.js
@@ -1,3 +1,9 @@
-export default function(req, res){
-  res.status(200).send("server endpoint")
+const handler = function(event, ctx) {
+  return {
+    statusCode: 200,
+    body: "server endpoint"
+  }
+}
+export {
+  handler
 }

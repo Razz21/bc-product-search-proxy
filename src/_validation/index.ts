1212e4b761bd8e829affbe48f3ffc8a7bbaf6422
Andrzej K <32792205+Razz21@users.noreply.github.com>
2022-07-02 00:26:49 +0200

    chore: update json validation coerce

--- src/_validation/index.ts
+++ src/_validation/index.ts
@@ -1,5 +1,5 @@
-import joiRoot from 'joi';
-import type { Root, ArraySchema, ObjectSchema, CustomHelpers, Extension } from 'joi';
+import joiRoot from "joi";
+import type { Root, ArraySchema, ObjectSchema, CustomHelpers, Extension } from "joi";
 
 interface ExtendedJoi extends Root {
   versionArray(): ArraySchema;
@@ -8,51 +8,63 @@ interface ExtendedJoi extends Root {
 
 const arrayExtension: Extension = {
   base: joiRoot.array(),
-  type: 'versionArray',
+  type: "versionArray",
   coerce: (value, helpers: CustomHelpers) => ({
-    value: value.split ? value.split(',') : value,
+    value: value.split ? value.split(",") : value,
   }),
-}
+};
 const jsonExtension: Extension = {
   base: joiRoot.object(),
-  coerce: (value: string|object, helpers: CustomHelpers) => {
-    try {
-      if(typeof value === "object"){
-        return { value }
+  coerce: {
+    from: "string",
+    method: (value: string, helpers: CustomHelpers) => {
+      try {
+        return { value: JSON.parse(value) };
+      } catch (_) {
+        return helpers.error("objectId.invalid");
       }
-      return { value: JSON.parse(value) };
-    } catch (_) {
-      return helpers.error('objectId.invalid');
-    }
+    },
   },
-  type: 'json',
+  type: "json",
   messages: {
-    'object.invalid': `"{{#label}}" must be a valid JSON`
+    "object.invalid": `"{{#label}}" must be a valid JSON`,
   },
-}
+};
 
-const joi: ExtendedJoi = joiRoot.extend(jsonExtension, arrayExtension)
+const joi: ExtendedJoi = joiRoot.extend(jsonExtension, arrayExtension);
 
-const headers = joi.object().keys({
-  'x-auth-token': joi.string().required(),
-  'store-hash': joi.string().required(),
-  'api-version': joi.string().optional(),
-  'content-type': joi.string().equal('application/json').optional(),
-}).unknown(true);
+const headers = joi
+  .object()
+  .keys({
+    "x-auth-token": joi.string().required(),
+    "store-hash": joi.string().required(),
+    "api-version": joi.string().optional(),
+    "content-type": joi.string().equal("application/json").required(),
+  })
+  .unknown(true);
 
 export default {
-  productSearch: joi.object({
-    headers,
-    body: joi.json().keys({
-      search_text: joi.string().trim().required(),
-      page: joi.number().integer().min(0).required(),
-      limit: joi.number().integer().positive().required(),
-    }).unknown(true)
-  }).unknown(true),
-  products: joi.object({
-    headers,
-    queryStringParameters: joi.object({
-      ids: joi.versionArray().items(joi.string()).required(),
-    }).unknown(true)
-  }).unknown(true)
-}
+  productSearch: joi
+    .object({
+      headers,
+      body: joi
+        .json()
+        .keys({
+          search_text: joi.string().trim().required(),
+          page: joi.number().integer().min(0).required(),
+          limit: joi.number().integer().positive().required(),
+        })
+        .unknown(true),
+    })
+    .unknown(true),
+  products: joi
+    .object({
+      headers,
+      queryStringParameters: joi
+        .object({
+          ids: joi.versionArray().items(joi.string()).required(),
+        })
+        .unknown(true),
+    })
+    .unknown(true),
+};
